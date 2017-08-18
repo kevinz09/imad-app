9f1bf46617f6854865f5f8283b7b88255d901217
kevinz09 <kevinthomas12589@gmail.com>
2017-08-18 14:41:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res) {
+    res.send(article-one requeste);
+});
+
+app.get('/article-two', function(req, res) {
+    res.send(article-two requeste);
+});
+
+app.get('/article-three', function(req, res) {
+    res.send(article-three requeste);
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
